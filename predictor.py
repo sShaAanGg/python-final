99f82a905e7a45a42e5b21aa64dea0c4cc5f254e
tsm55555 <57450036+tsm55555@users.noreply.github.com>
2022-05-24 17:56:47 +0800

    rearrange import

--- predictor.py
+++ predictor.py
@@ -1,7 +1,7 @@
-from tensorflow.keras.applications.resnet50 import ResNet50
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 
 model = ResNet50(weights='imagenet')
 
