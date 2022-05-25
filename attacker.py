413ad1337ebc3629e1352b8ececb931f4d42b162
tsm55555 <57450036+tsm55555@users.noreply.github.com>
2022-05-25 11:27:48 +0800

    rearrange attack.py's import

--- attacker.py
+++ attacker.py
@@ -1,20 +1,21 @@
+import cv2
+import json
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
 import torch
 import torch.nn as nn
-import json
 import torchvision.utils
-
 from torchvision import models
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from tensorflow.keras.preprocessing import image
-import cv2
+
 import torchattacks
-from predictor import predict
 from utils import image_folder_custom_label
+
+from predictor import predict
 
 # load data 
 class_idx = json.load(open("imagenet_class_index.json"))
@@ -60,9 +61,9 @@ model = model.eval()
 def attack(img):
     # attacker = torchattacks.BIM(model, eps=16/255, alpha=2/255, steps=100)
 
-    attacker = torchattacks.MIFGSM(model, eps=8/255, alpha=2/255, steps=100)
+    attacker = torchattacks.MIFGSM(model, eps=16/255, alpha=2/255, steps=100)
     # attacker.set_mode_targeted_least_likely(1)  # Targeted attack
-    attacker.set_mode_targeted_random()
+    # attacker.set_mode_targeted_random()
     # attacker = torchattacks.CW(model, c=1, lr=0.01, steps=100, kappa=0),
     # label, _, _ = predict(img)
     adv_images = attacker(img, labels)
@@ -82,9 +83,4 @@ def attack(img):
 
 
 if __name__ == '__main__':
-    # img_path = 'test_img/cat.jpg'
-    # img = image.load_img(img_path, target_size=(224, 224))
-    # img = image.img_to_array(img)
-    # img = np.expand_dims(img, axis=0)
-    # print(img.shape)
     attack(images)
